cd40dec94108ab4c4a26b91afbe048c1e387b848
Emil Galiyev <emil333.mem@gmail.com>
2024-04-08 06:04:03 +0600

    Минорные изменения

--- src/main/java/org/example/musicplayer03/Main.java
+++ src/main/java/org/example/musicplayer03/Main.java
@@ -7,12 +7,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class Module extends Application {
+public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Module.class.getResource("/org/example/musicplayer03/ui.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/org/example/musicplayer03/ui.fxml"));
 
-        Scene scene = new Scene(fxmlLoader.load(), 900, 400);
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setScene(scene);
         stage.setTitle("Music Player");
         stage.show();
