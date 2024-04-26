272afc46b47f963f9d16caebc895099ffcfae045
Adlett Anafiya <adlet.anafiya@mail.ru>
2024-04-26 13:37:13 +0500

    albums

--- src/main/java/org/example/musicplayer03/Controller.java
+++ src/main/java/org/example/musicplayer03/Controller.java
@@ -112,6 +112,8 @@ public class Controller implements Initializable {
     @FXML
     private ScrollPane PlaylistScrollPane;
     @FXML
+    private AnchorPane PlAnchor;
+    @FXML
     private ScrollPane ExamplePAne;
 @FXML
 private Button ClosePlbtn;
@@ -321,7 +323,7 @@ private TilePane ExampleTilePAne;
         Songs Strykalo = new Songs(4, "Kayen", "Strykalo", "Pop", "src/Music/Стрыкало/music.wav", "src/Music/Стрыкало/vocal.wav", "/icons/Смирись_и_расслабься!.jpg", "src/Lyrics/кайен.txt");
         Songs Kayrat_almaty = new Songs(5, "Алматынын тундеры", "Кайрат Нуртас", "Pop", "src/Music/Кайрош/Алматынын тундеры/music.wav", "src/Music/Кайрош/Алматынын тундеры/vocals.wav", "/icons/Kazakh.jpg", "src/Lyrics/Алматынын тундеры.txt");
         Songs Kayrat_myUniverse = new Songs(6, "My universe", "Кайрат Нуртас", "Pop", "src/Music/Кайрош/My Universe/My_Universe_music.wav", "src/Music/Кайрош/My Universe/My_Universe_vocals.wav", "/icons/Kazakh.jpg", "src/Lyrics/MyUniverse.txt");
-        Songs Rhapsody = new Songs(7,"Рапсодия конца света","GONE.fludd","Russia","src/Music/Рапсодия Конца Света/music.wav","src/Music/Рапсодия Конца Света/vocals.wav","/icons/Rapsodiya_Konca_Sveta.png","src/Lyrics/Rapsodiya.txt");
+        Songs Rhapsody = new Songs(7,"Рапсодия конца света","GONE.Fludd","Russia","src/Music/Рапсодия Конца Света/music.wav","src/Music/Рапсодия Конца Света/vocals.wav","/icons/Rapsodiya_Konca_Sveta.png","src/Lyrics/Rapsodiya.txt");
         Songs tesno = new Songs(8,"Тесно","Bushido Zho","Russia","src/Music/Тесно - Bushido ZHO/music.wav","src/Music/Тесно - Bushido ZHO/vocals.wav","/icons/Тесно.jpg","src/Lyrics/Тесно.txt");
         Songs domino = new Songs(9,"Домино","FACE","Russia","src/Music/Face - Домино/music.wav","src/Music/Face - Домино/vocals.wav","/icons/FACE.png","src/Lyrics/домино.txt");
         Songs MenSeniSuyemin = new Songs(10,"Men Seni Suyemin","Son Paskal","Kazakh","src/Music/Son Paskal - Men Seni Suyemin/music.wav","src/Music/Son Paskal - Men Seni Suyemin/vocals.wav","/icons/artworks-JwNW6K1GR42s-0-t500x500.jpg","src/Lyrics/men seni suyemin.txt");
@@ -725,6 +727,9 @@ private TilePane ExampleTilePAne;
 
                 PlaylistPane.getChildren().add(songButton);
             }
+
+
+
         }
 
 
@@ -823,7 +828,7 @@ private TilePane ExampleTilePAne;
             playlistButton.setMaxWidth(Double.MAX_VALUE);
 
             VBox outerVBox = new VBox(15);  // Внешний VBox для размещения кнопки с увеличенным отступом
-            outerVBox.setPadding(new Insets(20)); // Добавляем внешний отступ
+            outerVBox.setPadding(new Insets(30)); // Добавляем внешний отступ
             outerVBox.getChildren().addAll(playlistButton); // Добавляем только кнопку
             outerVBox.setAlignment(Pos.CENTER);  // Выравнивание элементов внутри VBox по центру
 
